ffa53ec7af7e9307d44742bb88370ec18675b576
Suman Mahato <sumanmahato0099@gmail.com>
2019-11-25 08:40:34 +0530

    secured private info

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-//Entry Point for Backend App
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+
+const User = require("./models/user");
 
 mongoose
-    .connect(
-        "mongodb+srv://suman0013:suman0013@react-blog-88tkt.mongodb.net/test?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.json({ hello: "world" });
+});
+
+app.post("/api/users/register", (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send(userData);
+    });
 });
 
 app.listen(5000, () => {
